cf00ba3139a667e6a9234f871e4d54a5b5a141c1
bjc1e <bjc1@kent.ac.uk>
2026-06-25 21:27:29 +0100

    Added SMB network share mounting and Bat

--- configuration.nix
+++ configuration.nix
@@ -125,7 +125,19 @@
     bibata-cursors
     pavucontrol
     tor-browser
+    cifs-utils
+    bat
 ];
+
+  fileSystems."/mnt/network_drive" = {
+    device = "//192.168.0.72/data";
+    fsType = "cifs";
+    options = let
+      # Prevent system hangs if the network is unavailable
+      automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+      
+    in ["${automount_opts},credentials=/etc/nixos/smb-secrets,uid=1000,gid=100"];
+  };
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   system.stateVersion = "26.05";
